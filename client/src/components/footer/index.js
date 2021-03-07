ccfbea805f040fbd1c6cd1f3625844f6aa0aa9e9
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-03-07 01:20:58 -0700

    commponents folders and files setup

---
+++ client/src/components/footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+ function Footer() {
+   return (
+     <footer>
+       <h3 class="nav-item">
+         <a class="nav-link" href="https://github.com/greg15323">Built by Greg Rodriguez</a>
+       </h3>
+       <h3 class="nav-item">
+         <a class="nav-link" href="https://github.com/Greg15323/Google_Book_Search.git">Site Repo</a> 
+       </h3>
+     </footer>
+   );
+ }
+
+ export default Footer;
